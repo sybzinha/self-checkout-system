fae43e15158aa18110dc20add8aa52a5cd1e3663
Sybilla Coppi <sybillafcoppi@gmail.com>
2025-02-22 02:51:03 -0300

    feat: ensure order can be created

---
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -0,0 +1,59 @@
+"use server";
+
+import { ConsumptionMethod } from "@prisma/client";
+
+import { db } from "@/lib/prisma";
+
+import { removeCpfPunctuation } from "../helpers/cpf";
+
+interface CreateOrderInput {
+  customerName: string;
+  custumerCpf: string;
+  products: Array<{
+    id: string;
+    quantity: number;
+  }>;
+  consumptionMethod: ConsumptionMethod;
+  slug: string;
+}
+
+export const createOrder = async (input: CreateOrderInput) => {
+  const restaurant = await db.restaurant.findUnique({
+    where: {
+      slug: input.slug,
+    },
+  });
+  if (!restaurant) {
+    throw new Error("Restaurant not found");
+  }
+  const productsWithPrices = await db.product.findMany({
+    where: {
+      id: {
+        in: input.products.map((product) => product.id),
+      },
+    },
+  });
+  const productsWithPricesAndQuantities = input.products.map((product) => ({
+    productId: product.id,
+    quantity: product.quantity,
+    price: productsWithPrices.find((p) => p.id === product.id)!.price,
+  }));
+  await db.order.create({
+    data: {
+      status: "PENDING",
+      custumerName: input.customerName,
+      custumerCpf: removeCpfPunctuation(input.custumerCpf),
+      orderProducts: {
+        createMany: {
+          data: productsWithPricesAndQuantities,
+        },
+      },
+      total: productsWithPricesAndQuantities.reduce(
+        (acc, product) => acc + product.price * product.quantity,
+        0,
+      ),
+      consumptionMethod: input.consumptionMethod,
+      restaurantId: restaurant.id,
+    },
+  });
+};
