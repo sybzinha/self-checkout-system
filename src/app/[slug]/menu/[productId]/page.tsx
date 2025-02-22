a8af87a673b7d7c3bebe5f28167a7f446138209d
Sybilla Coppi <sybillafcoppi@gmail.com>
2025-02-21 22:57:53 -0300

    feat: add product header component

---
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -0,0 +1,38 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ProductDetails from "./components/product-details";
+import ProductHeader from "./components/product-header";
+
+
+interface ProductPageProps {
+  params: Promise<{ slug: string; productId: string }>;
+}
+
+const ProductPage = async ({ params }: ProductPageProps) => {
+  const { productId } = await params;
+
+  const product = await db.product.findUnique({
+    where: { id: productId },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+          avatarImageUrl: true,
+        },
+      },
+    },
+  });
+  if (!product) {
+    return notFound();
+  }
+  return (
+    <div className="flex h-full flex-col">
+      <ProductHeader product={product} />
+      <ProductDetails product={product} />
+    </div>
+  );
+};
+
+export default ProductPage;
